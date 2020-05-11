458d4f8b9e84a9bb1881bd8bb7b3b29c38c8e3e3
suraj sahani <surajkumarsahani1997@gmail.com>
2020-05-11 13:40:26 +0530

    Updated Reverse string

--- string/ReverseString2.java
+++ string/ReverseString2.java
@@ -4,7 +4,9 @@ public class ReverseString2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		int principal = sc.nextInt();
+		float rate =sc.nextFloat();
+		int time =sc.nextInt();		
 	}
 
 }
