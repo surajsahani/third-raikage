001fdfc64398f94fff9aad5da8aacc5a9c541bc4
suraj sahani <surajkumarsahani1997@gmail.com>
2020-05-11 13:44:52 +0530

    Coder Branch change

--- loops/Break.java
+++ loops/Break.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class Break {
 
 	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		for(int i=1 ;i<=100 ;i++)
-//		{
-//			if (i==35) {
-//				break;
-//			}
-//			System.out.println(i);
-//		}
+	for(int i=1 ;i<=100 ;i++)
+	{
+		if (i==35) {
+			break;
+		}
+			System.out.println(i);
+		}
 	Scanner sc = new Scanner(System.in);
 	
 	for(;;) {
