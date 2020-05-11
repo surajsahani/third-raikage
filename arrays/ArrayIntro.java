2eb897ac3e7a0e843147dd4f458ce02a2ca6a571
suraj sahani <surajkumarsahani1997@gmail.com>
2020-05-11 12:36:11 +0530

    Bug fixing

--- arrays/ArrayIntro.java
+++ arrays/ArrayIntro.java
@@ -20,17 +20,17 @@ public class ArrayIntro {
 		
 		int[] age = {2,5,1,34,12};
 		
-//		double[] marks = {1.0,3.14,2.9};
-//		
-//		marks[1] = 4.56;
-//		
-//		System.out.println(marks[1]);
+		double[] marks = {1.0,3.14,2.9};
+	
+		marks[1] = 4.56;
+	
+	System.out.println(marks[1]);
 		
-//		System.out.println(age[0]);
-//		System.out.println(age[1]);
-//		System.out.println(age[2]);
-//		System.out.println(age[3]);
-//		System.out.println(age[4]);
+	System.out.println(age[0]);
+	System.out.println(age[1]);
+	System.out.println(age[2]);
+	System.out.println(age[3]);
+	System.out.println(age[4]);
 		
 		for(int i = 0; i<age.length; i++) {
 			System.out.println(age[i]);
