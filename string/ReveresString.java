978ee8460acf318ebff0267714bba2e580c06bca
suraj sahani <surajkumarsahani1997@gmail.com>
2020-05-11 13:24:14 +0530

    string/ReverseString2.java

--- string/ReveresString.java
+++ string/ReveresString.java
@@ -3,7 +3,7 @@ package string;
 public class ReveresString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- string/ReverseString2.java
+++ string/ReverseString2.java
@@ -1,10 +1,10 @@
 package string;
-
+import java.util.Scanner;
 public class ReverseString2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
 	}
 
 }
