c322ad755c2a0191fb8f3f6a89b7df7e9c2a0595
suraj sahani <surajkumarsahani1997@gmail.com>
2020-05-11 15:26:26 +0530

    Testing megrging

--- loops/Break.java
+++ loops/Break.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Break {
 
-<<<<<<< HEAD
+
 	public static void main(String[] args) {
 	for(int i=1 ;i<=100 ;i++)
 		{
@@ -14,26 +14,23 @@ public class Break {
 		System.out.println(i);
 	}
 	Scanner sc = new Scanner(System.in);
-=======
-//	public static void main(String[] args) {
-//	for(int i=1 ;i<=100 ;i++)
-//	{
-//		if (i==35) {
-//			break;
-//		}
-//			System.out.println(i);
-//		}
-//	Scanner sc = new Scanner(System.in);
->>>>>>> ba360de1d001250a540a394c4d2c4c5db7060343
 	
-	for(;;) {
+	for(int i=1 ;i<=100 ;i++)
+	{
+		if (i==35) {
+			break;
+		}
+			System.out.println(i);
+		}
+
+	
+	//for(;;) {
 		int n = sc.nextInt();
 				
 				if(n<0) {
-					break;
+					//break;
 				}
-	
 	}
+}
+	
 
-}
-}
