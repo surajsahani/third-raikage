ba360de1d001250a540a394c4d2c4c5db7060343
suraj sahani <surajkumarsahani1997@gmail.com>
2020-05-11 13:46:37 +0530

    Working with branches

--- loops/Break.java
+++ loops/Break.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Break {
 
-	public static void main(String[] args) {
-	for(int i=1 ;i<=100 ;i++)
-	{
-		if (i==35) {
-			break;
-		}
-			System.out.println(i);
-		}
-	Scanner sc = new Scanner(System.in);
+//	public static void main(String[] args) {
+//	for(int i=1 ;i<=100 ;i++)
+//	{
+//		if (i==35) {
+//			break;
+//		}
+//			System.out.println(i);
+//		}
+//	Scanner sc = new Scanner(System.in);
 	
 	for(;;) {
 		int n = sc.nextInt();
